82125c4a01fce39e813f36bd709c70dc0553062b
Jo Young <githuborigin@gmail.com>
2023-04-24 09:15:56 +0900

    [ADD] PDF 파일 업로드 기능 구현 #4

--- src/components/Pdf/UplodPDF.tsx
+++ src/components/Pdf/UplodPDF.tsx
@@ -6,13 +6,21 @@ import LoadingIndicator from './LoadingIndicator';
 
 const UplodPDF = () => {
   const [PDFfile, setPDFfile] = useState<File | null>(null);
+  const [isUploading, setIsUploading] = useState<boolean>(false);
 
   const onFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setPDFfile(event.target.files ? event.target.files[0] : null);
   };
 
   const onFileUpload = async () => {
+    if (!PDFfile) {
+      console.log('PDF 파일이 선택되지 않았습니다.');
+      return;
+    }
+    setIsUploading(true);
+
     const formData = new FormData();
+
     if (PDFfile instanceof File)
       formData.append('multipartFile', PDFfile as File, encodeURIComponent(PDFfile.name));
     console.log('서버 pdf 요청 ');
@@ -24,15 +32,24 @@ const UplodPDF = () => {
     } catch (error) {
       // 필요하다면 사용자에게 에러 메시지를 표시합니다.
       console.error('파일 업로드 실패: ', error);
+    } finally {
+      setIsUploading(false); // 파일 업로드 요청이 종료됨을 표시
     }
   };
+  const handledDeletePDFfile = (): void => {
+    setPDFfile(null);
 
+    console.error('삭제: ', PDFfile);
+  };
   return (
     <>
       <h1>PDF 업로드</h1>
       {}
-      <input type="file" onChange={onFileChange} />
-      <Button onClick={onFileUpload}>업로드</Button>
+      <input type="file" onChange={onFileChange} disabled={isUploading} />
+      <Button onClick={handledDeletePDFfile}>삭제</Button>
+      <Button onClick={onFileUpload} disabled={isUploading}>
+        업로드
+      </Button>
       <LoadingIndicator />
     </>
   );
