f152bb1a77c81627d27e93c3d785495331170ba8
sihyun10 <hyhy200170@gmail.com>
2023-04-24 23:42:07 +0900

    Feat: 드래그앤드롭 기능추가
    Pdf폴더 안에 DragAndDrop.tsx파일을 생성함. Pra페이지폴더안에 DragAndDrop
    컴포넌트 추가해뒀습니다.
    드래그앤드롭 이벤트를 구현하기 위해 4개의 이벤트가 있고, 컴포넌트가
    언마운트 되었을때, 등록된 이벤트들을 지워줌. (그 이유는 브라우저에서
    기본으로 등록된 이벤트 동작이 발생하기 때문에)

--- src/pages/Pra/PdfUpload.tsx
+++ src/pages/Pra/PdfUpload.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import ViewPDF from '../../components/Pdf/ViewPDF';
 import Upload from '../../components/Pdf/UplodPDF';
+import DragAndDrop from '@/components/Pdf/DragAndDrop';
 
 //  업로드 상태변화를 감지하여 페이지를 이동해주는 컴포넌트
 const PRA = () => {
@@ -9,6 +10,8 @@ const PRA = () => {
       <h1>PRA 페이지입니다. 이곳에서 PDF를 업로드해주세요.</h1>
       <Upload />
       <ViewPDF />
+      {/* 드래그앤드롭모듈입니다. */}
+      <DragAndDrop />
     </>
   );
 };
