88d4b1a11c3021b30037f59e63577dd41a2249ab
Jo Young <githuborigin@gmail.com>
2023-04-28 21:27:23 +0900

    fix: common 폴더수정

--- src/components/Comon/PrimaryModal.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const PrimaryModal = () => {
-  return <div>PrimaryModal</div>;
-};
-
-export default PrimaryModal;
--- src/components/Comon/index.tsx
+++
@@ -1,3 +0,0 @@
-export { default as PrimaryButton } from './PrimaryButton';
-export { default as SpinnerButton } from './SpinnerButton';
-export { default as CancelButton } from './CancleButton';
