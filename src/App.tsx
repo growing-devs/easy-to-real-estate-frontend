892d5e493f7de1debdd86d44640c64836748e743
Jo Young <githuborigin@gmail.com>
2023-05-05 22:13:39 +0900

    병합

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,7 @@ import Layout from './components/Layout';
 import Home from './pages/Home';
 import Pra from './pages/Pra/PraUpload';
 import PraDetail from './pages/Pra/PraDetail';
-
-import Simple from './pages/Simple';
+import Search from './pages/Search';
 import News from './pages/News';
 import Support from './pages/Support';
 import NotFound from './pages/NotFound';
@@ -17,8 +16,7 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/pra" element={<Pra />} />
         <Route path="/pra/detail" element={<PraDetail />} />
-
-        <Route path="/simple" element={<Simple />} />
+        <Route path="/search" element={<Search />} />
         <Route path="/news" element={<News />} />
         <Route path="/support" element={<Support />} />
         <Route path="*" element={<NotFound />} />
---
+++ src/components/HomeComp/HomeMenus/index.tsx
@@ -0,0 +1,18 @@
+import { Link } from 'react-router-dom';
+import RadiusBox from '../../common/RadiusBox';
+import { HomeMenusContainer, HomeMenuIcon } from './style';
+
+const HomeMenus = () => {
+  return (
+    <HomeMenusContainer>
+      <Link to="/pra" className="homeMain">
+        심사 시작하기
+      </Link>
+      <Link to="/search" className="homeSub">
+        등기부등본이 없으시다면?
+      </Link>
+    </HomeMenusContainer>
+  );
+};
+
+export default HomeMenus;
