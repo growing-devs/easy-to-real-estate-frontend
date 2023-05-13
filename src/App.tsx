f8fd549c3b24b49ca35dcc44ecbd36b83c210a22
05Castle <kos4005@gmail.com>
2023-05-13 22:20:28 +0900

    Refactor: 레이아웃 컴포넌트 하나로 합치기 #42
    따로 나뉘어져 있던 전역 레이아웃과, pra 화면 전용 레이아웃인
    ReviewLayout을 gird를 써서 하나로 합침

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,11 @@ import { Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
 import Home from './pages/Home';
 import Pra from './pages/Pra/PraUpload';
-import PraDetail from './pages/Pra/PraDetail';
 import PraPrice from './pages/Pra/PraPrice';
 
 import MyReviews from './pages/MyReviews';
 import Search from './pages/Search';
 import NotFound from './pages/NotFound';
-import Chart from './components/Chart';
-import ReviewLayout from './components/ReviewLayout';
 import { PdfSummary, PdfGap, PdfEul, MarketPrice, Location } from './pages/Detailed';
 
 const App = () => {
@@ -18,13 +15,12 @@ const App = () => {
       <Route path="/" element={<Home />} />
       <Route element={<Layout />}>
         <Route path="pra" element={<Pra />} />
-        <Route path="review/:id" element={<ReviewLayout />}>
-          <Route path="pdfsummary" element={<PdfSummary />} />
-          <Route path="gap" element={<PdfGap />} />
-          <Route path="eul" element={<PdfEul />} />
-          <Route path="marketprice" element={<MarketPrice />} />
-          <Route path="location" element={<Location />} />
-        </Route>
+        <Route path="review/:id/pdfsummary" element={<PdfSummary />} />
+        <Route path="review/:id/gap" element={<PdfGap />} />
+        <Route path="review/:id/eul" element={<PdfEul />} />
+        <Route path="review/:id/marketprice" element={<MarketPrice />} />
+        <Route path="review/:id/location" element={<Location />} />
+
         <Route path="/praprice" element={<PraPrice />} />
         <Route path="/myreviews" element={<MyReviews />} />
         <Route path="/search" element={<Search />} />
