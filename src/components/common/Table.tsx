27d37af8a83134b8ea0897f8ed2b2863f9a73b48
05Castle <kos4005@gmail.com>
2023-05-15 18:01:39 +0900

    Conf: 테스트용 토이 서버 구축을 위한 mockup 데이터 생성
    json-server 라이브러리 devDependencies로 설치
    mockupdb.json 생성
    
    server scripts 포트 4000으로 세팅

--- src/components/common/Table.tsx
+++ src/components/common/Table.tsx
@@ -6,21 +6,41 @@ export interface TableProps {
   tableData: any[]; // 테이블에 표시할 데이터 배열
   tableColumns: Column<any>[]; // 테이블에 표시할 컬럼 설정 배열
   maxHeight?: string; // 테이블 높이 설정 (default: '200px')
+  disableScroll?: boolean; // 스크롤 제거 여부 (default: false 스크롤 있음)
+  width?: string[]; // 각 열의 너비값 배열 (tableColumns 배열의 순서와 일치해야함)
 }
 
-const Table: React.FC<TableProps> = ({ tableData, tableColumns, maxHeight = '200px' }) => {
+const Table: React.FC<TableProps> = ({
+  tableData,
+  tableColumns,
+  maxHeight = '200px',
+  disableScroll = false,
+  width = [],
+}) => {
   const data = useMemo(() => tableData, [tableData]);
-  const columns = useMemo(() => tableColumns, [tableColumns]);
+  const columns = useMemo(
+    () =>
+      tableColumns.map((column, index) => ({
+        ...column,
+        width: width ? width[index] : '100%',
+      })),
+    [tableColumns, width],
+  );
+
   const tableInstance = useTable({ columns, data });
 
   return (
-    <TableWrapper maxHeight={maxHeight}>
+    <TableWrapper maxHeight={maxHeight} disableScroll={disableScroll}>
       <table>
         <TableHeader>
           {tableInstance.headerGroups.map((headerGroup) => (
             <tr {...headerGroup.getHeaderGroupProps()} key={headerGroup.id}>
-              {headerGroup.headers.map((column) => (
-                <th {...column.getHeaderProps()} key={column.id}>
+              {headerGroup.headers.map((column, index) => (
+                <th
+                  {...column.getHeaderProps()}
+                  key={column.id}
+                  style={{ width: width ? width[index] : undefined }}
+                >
                   {column.render('Header')}
                 </th>
               ))}
@@ -48,14 +68,15 @@ const Table: React.FC<TableProps> = ({ tableData, tableColumns, maxHeight = '200
 
 Table.defaultProps = {
   maxHeight: '200px',
+  disableScroll: false,
+  width: [],
 };
 
 export default Table;
 
-const TableWrapper = styled.div<{ maxHeight?: string }>`
-  margin: 1rem;
+const TableWrapper = styled.div<{ maxHeight?: string; disableScroll?: boolean }>`
   max-height: ${({ maxHeight }) => maxHeight || '200px'};
-  overflow-y: scroll;
+  overflow-y: ${({ disableScroll }) => (disableScroll ? 'hidden' : 'scroll')};
 `;
 
 const TableHeader = styled.thead`
