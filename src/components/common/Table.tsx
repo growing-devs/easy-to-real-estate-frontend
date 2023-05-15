207983d234a31fee7df725a0143f500b6ce59000
05Castle <kos4005@gmail.com>
2023-05-15 17:17:05 +0900

    init commit before push

--- src/components/common/Table.tsx
+++ src/components/common/Table.tsx
@@ -6,41 +6,21 @@ export interface TableProps {
   tableData: any[]; // 테이블에 표시할 데이터 배열
   tableColumns: Column<any>[]; // 테이블에 표시할 컬럼 설정 배열
   maxHeight?: string; // 테이블 높이 설정 (default: '200px')
-  disableScroll?: boolean; // 스크롤 제거 여부 (default: false 스크롤 있음)
-  width?: string[]; // 각 열의 너비값 배열 (tableColumns 배열의 순서와 일치해야함)
 }
 
-const Table: React.FC<TableProps> = ({
-  tableData,
-  tableColumns,
-  maxHeight = '200px',
-  disableScroll = false,
-  width = [],
-}) => {
+const Table: React.FC<TableProps> = ({ tableData, tableColumns, maxHeight = '200px' }) => {
   const data = useMemo(() => tableData, [tableData]);
-  const columns = useMemo(
-    () =>
-      tableColumns.map((column, index) => ({
-        ...column,
-        width: width ? width[index] : '100%',
-      })),
-    [tableColumns, width],
-  );
-
+  const columns = useMemo(() => tableColumns, [tableColumns]);
   const tableInstance = useTable({ columns, data });
 
   return (
-    <TableWrapper maxHeight={maxHeight} disableScroll={disableScroll}>
+    <TableWrapper maxHeight={maxHeight}>
       <table>
         <TableHeader>
           {tableInstance.headerGroups.map((headerGroup) => (
             <tr {...headerGroup.getHeaderGroupProps()} key={headerGroup.id}>
-              {headerGroup.headers.map((column, index) => (
-                <th
-                  {...column.getHeaderProps()}
-                  key={column.id}
-                  style={{ width: width ? width[index] : undefined }}
-                >
+              {headerGroup.headers.map((column) => (
+                <th {...column.getHeaderProps()} key={column.id}>
                   {column.render('Header')}
                 </th>
               ))}
@@ -68,15 +48,14 @@ const Table: React.FC<TableProps> = ({
 
 Table.defaultProps = {
   maxHeight: '200px',
-  disableScroll: false,
-  width: [],
 };
 
 export default Table;
 
-const TableWrapper = styled.div<{ maxHeight?: string; disableScroll?: boolean }>`
+const TableWrapper = styled.div<{ maxHeight?: string }>`
+  margin: 1rem;
   max-height: ${({ maxHeight }) => maxHeight || '200px'};
-  overflow-y: ${({ disableScroll }) => (disableScroll ? 'hidden' : 'scroll')};
+  overflow-y: scroll;
 `;
 
 const TableHeader = styled.thead`
@@ -94,12 +73,8 @@ const TableBody = styled.tbody`
   tr:nth-of-type(odd) {
     background-color: #fff;
   }
-  td {
-    text-align: center;
-  }
 `;
 
 const TableCell = styled.td`
   padding: 1rem;
-  text-align: center;
 `;
