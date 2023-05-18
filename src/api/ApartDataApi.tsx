d8adbed6679722de704167386e3d0bdfc54cb49f
Jo Young <githuborigin@gmail.com>
2023-05-19 01:09:22 +0900

    fix: cors오류해결

--- src/api/ApartDataApi.tsx
+++ src/api/ApartDataApi.tsx
@@ -27,7 +27,9 @@ const fetchData = async (lawCityNumber: string, yyyymm: string, pageNo = 1): Pro
   queryParams += `&LAWD_CD=${lawCityNumber}`;
   queryParams += `&DEAL_YMD=${yyyymm}`;
 
-  const response = await axios.get(`/proxy?${queryParams}`);
+  const url = `/proxy?${queryParams}`;
+  console.log('Request URL:', url);
+  const response = await axios.get(url);
   const responseData = response.data.response.body.items.item;
   const { totalCount } = response.data.response.body;
   if (totalCount > pageNo * 49) {
